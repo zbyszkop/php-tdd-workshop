8f3549cdc2b19705968e652217d3d776a4d5eb7d
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-03-23 20:37:46 +0100

    Up until tests for strike

--- src/BowlingScorer.php
+++ src/BowlingScorer.php
@@ -3,8 +3,42 @@
 
 class BowlingScorer
 {
-    function score(int $score): void 
+
+    private int $score = 0;
+    private bool $firstThrow = true;
+    private int $frameScore = 0;
+    private bool $spare = false;
+
+    public function roll(int $roll)
     {
+        if ($this->firstThrow) {
+            if ($this->spare) {
+                $this->spare = false;
+                $this->addScore(10 + $roll);
+            }
+            $this->frameScore += $roll;
+            $this->firstThrow = false;
+        } else {
+            if ($roll + $this->frameScore == 10) {
+                $this->spare = true;
+            } else {
+                $this->addScore($roll + $this->frameScore);
+            }
+            $this->frameScore = 0;
+            $this->firstThrow = true;
+        }
+
+    }
+
+    private function addScore(int $frameScore) {
+        $this->score += $frameScore;
+    }
+
+    public function getScore()
+    {
+
+
+        return $this->score;
 
     }
 }
--- tests/unit/BowlingScorerTest.php
+++ tests/unit/BowlingScorerTest.php
@@ -4,7 +4,6 @@ use PHPUnit\Framework\TestCase;
 
 final class BowlingScorerTest extends TestCase
 {
-
     private $bowlingScorer;
 
     protected function setUp(): void
@@ -15,8 +14,53 @@ final class BowlingScorerTest extends TestCase
 
     public function testShouldBeAbleToScoreAndSeeScore(): void
     {
-        $this->bowlingScorer->score(0);
-        $this->assertTrue(true);
+        ($this->bowlingScorer)->roll(0);
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+        ($this->bowlingScorer)->roll(4);
+        $this->assertEquals(4, $this->bowlingScorer->getScore());
     }
 
+    public function testPointsShouldBeTalliedAfterAFrame(): void
+    {
+        ($this->bowlingScorer)->roll(4);
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+        ($this->bowlingScorer)->roll(4);
+        $this->assertEquals(8, $this->bowlingScorer->getScore());
+    }
+
+    public function testSpare(): void {
+        ($this->bowlingScorer)->roll(4);
+        ($this->bowlingScorer)->roll(6);
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+        ($this->bowlingScorer)->roll(5);
+        $this->assertEquals(15, $this->bowlingScorer->getScore());
+    }
+    
+    public function testFullZeroGame(): void {
+        for ($i = 0; $i<20; $i++) {
+            ($this->bowlingScorer)->roll(0);
+        }
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+    }
+
+    public function testFullRollsGame(): void {
+        for ($i = 0; $i<20; $i++) {
+            ($this->bowlingScorer)->roll(4);
+        }
+        $this->assertEquals(80, $this->bowlingScorer->getScore());
+    }
+
+    public function testFullSparesGame(): void {
+        for ($i = 0; $i<21; $i++) {
+            ($this->bowlingScorer)->roll(5);
+        }
+        $this->assertEquals(150, $this->bowlingScorer->getScore());
+    }
+
+    public function testAStrike(): void {
+        ($this->bowlingScorer)->roll(10);
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+        ($this->bowlingScorer)->roll(3);
+        $this->assertEquals(0, $this->bowlingScorer->getScore());
+    }
 }
